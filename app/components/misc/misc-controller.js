5126fa75deeb188e3e7c3e44226633dbee354aa8
Allen Buckley <abuckley@fusionalliance.com>
2015-09-12 15:25:14 -0400

    Minor udpates

--- app/components/misc/misc-controller.js
+++ app/components/misc/misc-controller.js
@@ -6,21 +6,26 @@ class ArrowController {
         forOf();
 
         function forOf () {
-            var arr = [2, 4, 6];
-            var result = '';
+            let arr = [2, 4, 6];
+            let result = '';
 
             // new - for-of
             for(let elem of arr) {
                 result += elem + ' ';
             }
 
-
             //for-of would show 2 4 6 and for-in would show 0 1 2 
             //and any other property name. If there was an arr.foo
             //for-in would have a result of 0 1 2 foo
             
-            
             vm.forOf = result;
+
+            //includes
+            let name = 'Josh Smith';
+            vm.includes = name.includes('mith');
+
+            vm.startsWith = name.startsWith('Josh');
+            vm.endsWith = name.endsWith('th');
         }
     }
 }
